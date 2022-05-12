d2270c797243373747954805e479eb058a7f6de9
Ramu Makkena <MRamuMailer@gmail.com>
2022-05-12 18:06:17 -0500

    saving local

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
+const passport = require('passport')
+router.get('/profile', passport.authenticationMiddleware(), renderProfile)
+  
 
+router.post('/login', (req, res) => {
+    // find all categories
+    // be sure to include its associated Products
+    Category.findAll({
+      include:[
+        {model: Product}
+      ]
+    })
+    .then(dbCategoriesData => res.json(dbCategoriesData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
 module.exports = router;
