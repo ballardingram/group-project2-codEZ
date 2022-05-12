b7bd39e584689d907576fc37f39e3688cf159b40
Ballard Ingram <ballardingram@outlook.com>
2022-05-12 09:12:57 -0500

    db/db.json - added - starting routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,14 @@
+// ROUTES > EXTERNAL ROUTES AND DEPENDENCIES
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const {tips} = require("../../db/db.json");
 router.use('/users', userRoutes);
+
+
+//ROUTES > GET ALL EXISTING TIPS
+router.get("/tips", (req, res) => {
+    let results = tips;
+    res.json(results);
+});
 
 module.exports = router;
